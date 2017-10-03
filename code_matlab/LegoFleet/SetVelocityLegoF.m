c067f5da8455135c5c0b91152ce4664651bdcf68
Joel <joel.daouk@univ-lorraine.fr>
2017-10-03 14:54:44 +0200

    destruction de l'objet current_robot après envoie des instructions

--- code_matlab/LegoFleet/SetVelocityLegoF.m
+++ code_matlab/LegoFleet/SetVelocityLegoF.m
@@ -3,5 +3,6 @@ for i=1:LegoFleet.NumberOfLegoRobots
      LegoName = char(LegoFleet.LegoNameArray(i));
      current_robot = ConnectLego(LegoName);
      SetVelocityOneLego(current_robot);
+     clear current_robot;
 end
 end
